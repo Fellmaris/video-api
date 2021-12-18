4eaa7500e7fb24ec891aa177b32b3f84e3e81cca
Fellmaris <cerniauskas.arturas@gmail.com>
2021-12-18 19:15:03 +0200

    fixed playlist load from local storage

--- src/main/java/com/examples/ApiPath.java
+++ src/main/java/com/examples/ApiPath.java
@@ -2,6 +2,7 @@ package com.examples;
 
 public interface ApiPath {
     String ID_VARIABLE = "id";
+
     String ROOT = "/api";
     String VIDEOS = "/videos";
     String VIDEO = "/{" + ID_VARIABLE + "}";
